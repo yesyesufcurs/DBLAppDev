89dee6506d7eb4c879c355d4f1032346dceb0be9
Stijn-Kuijper <stijnkuijper@hetnet.nl>
2021-02-14 16:31:39 +0100

    Changed layouts into actual input fields and buttons
    Still have to figure out how to get the styling done on these though. Seems to be working with an internal styling that is preset to purple and teal colors.

--- app/src/main/java/com/dblappdev/app/MainActivity.java
+++ app/src/main/java/com/dblappdev/app/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); // hide the title bar
+        setContentView(R.layout.login);
     }
 }
