6f11096f0ce9d539559528a4e25e89462340969d
yesyesufcurs <31587952+yesyesufcurs@users.noreply.github.com>
2021-02-22 14:04:27 +0100

    Added functions for testing purposes

--- backend/backendserver/__init__.py
+++ backend/backendserver/__init__.py
@@ -11,8 +11,8 @@ app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 CORS(app)
 
 # Path to db. Change accordingly
-db_file = "/home/vincent/Documents/DBLAppDev/backend/backendserver/app.db"
-# db_file = "/mnt/c/Users/Vincent/AndroidStudioProjects/App/backend/backendserver/app.db"
+# db_file = "/home/vincent/Documents/DBLAppDev/backend/backendserver/app.db"
+db_file = "/mnt/c/Users/Vincent/AndroidStudioProjects/App/backend/backendserver/app.db"
 
 def create_connection(db_file):
     conn = None
@@ -30,6 +30,7 @@ import backendserver.expense_group
 def home():
     return "Backend running normally"
 
+### WARNING: Methods underneath are for internal use, do not push to production
 @app.route("/showAllUsers")
 def showAllUsers():
 
@@ -47,4 +48,70 @@ def showAllUsers():
     result = cursor.fetchall()
     return jsonify(result)
 
+@app.route("/showAllExpenseGroups")
+def showAllExpenseGroups():
 
+    # create a database connection
+    cursor, connection = None, None
+    
+    try:
+        connection = create_connection(db_file)
+        cursor = connection.cursor()
+    except Exception as e:
+        return jsonify(error=500, text="could not connect to database"), 500
+    
+    query = "SELECT * FROM expense_group"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return jsonify(result)
+
+@app.route("/showAllExpenses")
+def showAllExpenses():
+
+    # create a database connection
+    cursor, connection = None, None
+    
+    try:
+        connection = create_connection(db_file)
+        cursor = connection.cursor()
+    except Exception as e:
+        return jsonify(error=500, text="could not connect to database"), 500
+    
+    query = "SELECT * FROM expense"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return jsonify(result)
+
+@app.route("/showAllAccuredExpenses")
+def showAllAccuredExpenses():
+
+    # create a database connection
+    cursor, connection = None, None
+    
+    try:
+        connection = create_connection(db_file)
+        cursor = connection.cursor()
+    except Exception as e:
+        return jsonify(error=500, text="could not connect to database"), 500
+    
+    query = "SELECT * FROM accured_expenses"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return jsonify(result)
+
+@app.route("/showAllExpenseGroupMembers")
+def showAllExpenseGroupMembers():
+
+    # create a database connection
+    cursor, connection = None, None
+    
+    try:
+        connection = create_connection(db_file)
+        cursor = connection.cursor()
+    except Exception as e:
+        return jsonify(error=500, text="could not connect to database"), 500
+    
+    query = "SELECT * FROM expense_group_members"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return jsonify(result)
