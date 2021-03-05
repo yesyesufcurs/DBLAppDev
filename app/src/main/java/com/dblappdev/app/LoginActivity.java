7eb66e5b11d591dc60534eec83ac683ab996e617
Stijn-Kuijper <stijnkuijper@hetnet.nl>
2021-03-05 16:32:10 +0100

    Removed old layout and added button handlers
    The methods 'onLoginClick' and 'onRegisterClick' handle the events for the login button and register button respectively.

--- app/src/main/java/com/dblappdev/app/LoginActivity.java
+++ app/src/main/java/com/dblappdev/app/LoginActivity.java
@@ -2,7 +2,9 @@ package com.dblappdev.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +13,21 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_login);
+    }
+
+    /**
+     * Event handler for the Login button on the login screen
+     * @param view The View instance of the button that was pressed
+     */
+    public void onLoginClick(View view) {
+
+    }
+
+    /**
+     * Event handler for the Register button on the login screen
+     * @param view The View instance of the button that was pressed
+     */
+    public void onRegisterClick(View view) {
+
     }
 }
