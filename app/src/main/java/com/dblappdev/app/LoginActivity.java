53d5f40eb16478ab159b452363615fb226bee9f7
Stijn-Kuijper <stijnkuijper@hetnet.nl>
2021-03-05 15:21:52 +0100

    Implemented splash screen and login screen
    Introduced a 2s delay for now. Could be changed depending on the requirements we set. Login screen now works for both portrait and landscape on phones and tablets.

---
+++ app/src/main/java/com/dblappdev/app/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.dblappdev.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // hide the title bar
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/dblappdev/app/MainActivity.java
+++ app/src/main/java/com/dblappdev/app/MainActivity.java
@@ -2,7 +2,9 @@ package com.dblappdev.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide(); // hide the title bar
-        setContentView(R.layout.login);
+         setContentView(R.layout.activity_main);   // set splash screen
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
+//        setContentView(R.layout.login);
     }
 }
