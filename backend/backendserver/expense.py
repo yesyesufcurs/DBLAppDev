401a885e30557b9c335c069de325b1dffbe30d6a
Vincent <vincentjanssen2001@gmail.com>
2021-03-12 12:42:08 +0100

    text recognition add

--- backend/backendserver/expense.py
+++ backend/backendserver/expense.py
@@ -592,10 +592,10 @@ def detect_text(content):
     for text in texts:
         print('\n"{}"'.format(text.description))
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
+        # vertices = (['({},{})'.format(vertex.x, vertex.y)
+        #             for vertex in text.bounding_poly.vertices])
 
-        print('bounds: {}'.format(','.join(vertices)))
+        # print('bounds: {}'.format(','.join(vertices)))
 
     if response.error.message:
         raise Exception(
