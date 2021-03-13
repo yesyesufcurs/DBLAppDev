c6302766b2e98c6dadab19ac4c08835f6431a277
Vincent <vincentjanssen2001@gmail.com>
2021-03-13 16:41:43 +0100

    Error fixed

--- app/src/main/java/com/dblappdev/app/MainActivity.java
+++ app/src/main/java/com/dblappdev/app/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.StrictMode;
 
 public class MainActivity extends AppCompatActivity {
 
