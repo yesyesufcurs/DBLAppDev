2750cd1374bbd7da29a76644109aa6d6a5edea1c
TOFenrir <38751837+TOFenrir@users.noreply.github.com>
2021-03-24 13:26:35 +0100

    Set id to settings head

--- app/src/main/java/com/dblappdev/app/GroupSettingsActivity.java
+++ app/src/main/java/com/dblappdev/app/GroupSettingsActivity.java
@@ -7,15 +7,27 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.dblappdev.app.adapters.MemberBalanceAdapter;
 
 public class GroupSettingsActivity extends AppCompatActivity {
+    int expenseGroupID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_settings);
+
+        // Get the linked expense group ID and check if it was properly defined
+        Bundle bundle = getIntent().getExtras();
+        if (!getIntent().hasExtra("EXPENSE_GROUP_ID")) {
+            throw new RuntimeException("Something went wrong with opening the expense group: no " +
+                    "expense group selected.");
+        }
+        expenseGroupID = bundle.getInt("EXPENSE_GROUP_ID");
+
+        ((TextView) findViewById(R.id.groupNameText)).setText(expenseGroupID);
 
         // Set the recyclerview and its settings
         RecyclerView recView = (RecyclerView) findViewById(R.id.recyclerViewMembersBalance);
