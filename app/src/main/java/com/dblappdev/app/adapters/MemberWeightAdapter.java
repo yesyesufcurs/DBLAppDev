a29f3b738a782eef5d7154631a3d9b2ee817308f
Richard <richardvanwouw@hotmail.com>
2021-04-12 15:32:58 +0200

    Changed MemberWeightAdapter to adhere to coding standard

--- app/src/main/java/com/dblappdev/app/adapters/MemberWeightAdapter.java
+++ app/src/main/java/com/dblappdev/app/adapters/MemberWeightAdapter.java
@@ -48,8 +48,11 @@ public class MemberWeightAdapter extends RecyclerView.Adapter<MemberWeightAdapte
     /**
      * Initialize the dataset of the adapter
      */
-    public MemberWeightAdapter(View.OnClickListener plusListener, View.OnClickListener minusListener,
-                               ArrayList<User> users, HashMap<User, Integer> amountMap) {
+    public MemberWeightAdapter(
+            View.OnClickListener plusListener,
+            View.OnClickListener minusListener,
+            ArrayList<User> users,
+            HashMap<User, Integer> amountMap) {
 
         this.userList = users;
         this.amountMap = amountMap;
@@ -63,7 +66,10 @@ public class MemberWeightAdapter extends RecyclerView.Adapter<MemberWeightAdapte
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         // Create a new view, which defines the UI of the list item
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_view_item_members, viewGroup, false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(
+                R.layout.recycler_view_item_members,
+                viewGroup,
+                false);
         // Bind the on click listeners for the plus and minus buttons
         view.findViewById(R.id.plus_button).setOnClickListener(plusListener);
         view.findViewById(R.id.minus_button).setOnClickListener(minusListener);
